8f34c3f008d2f66bcd59bf5781c5eade45a0a5d6
BuiTuanKhai15052001 <buituankhai2001@gmail.com>
2023-05-26 01:01:56 +0700

    fix: integrate mongodb cloud

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev');
+        await mongoose.connect('mongodb+srv://admin:admin@mern-learnit.xoudesc.mongodb.net/blog');
         console.log('Connect Successfully!');
     } catch (error) {
         console.log('Fail!');
